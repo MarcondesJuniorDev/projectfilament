641a0561905faf96731b361b62e0262058bc9bee
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-29 08:04:20 -0400

    Geração de dados fake para preenchimento da base de dados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Super',
+            'email' => 'super@super.com',
+            'password' => bcrypt('M4rc0nd35'),
         ]);
+
+        User::factory(100)->create();
     }
 }
