2776764f8aa740b7b27e592d1a68d273ca98a24a
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-30 16:11:16 -0400

    Geração de Resources e suas paginas de CRUD para o model Componentes Curriculares

---
+++ app/Filament/Resources/SubjectResource.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SubjectResource\Pages;
+use App\Filament\Resources\SubjectResource\RelationManagers;
+use App\Models\Subject;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SubjectResource extends Resource
+{
+    protected static ?string $model = Subject::class;
+
+    protected static ?string $navigationGroup = "L M S";
+
+    protected static ?string $modelLabel = "Componente Curricular";
+
+    protected static ?string $pluralModelLabel = "Componentes Curriculares";
+
+    protected static ?string $navigationIcon = 'heroicon-o-puzzle-piece';
+
+    protected static ?string $activeNavigationIcon = 'heroicon-s-puzzle-piece';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()
+                    ->description('Preencha o formulário de acordo com as informações solicitadas.')
+                    ->aside()
+                    ->schema([
+                        Forms\Components\TextInput::make('title')
+                            ->label('Título')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('code')
+                            ->label('Código Único')
+                            ->mask('aaa9999')
+                            ->placeholder('aaa0000')
+                            ->required()
+                            ->unique('subjects', 'code')
+                            ->maxLength(255),
+                        Forms\Components\Select::make('status')
+                            ->label('Status')
+                            ->options([
+                                'rascunho' => 'Rascunho',
+                                'publicado' => 'Publicado',
+                                'pendente' => 'Pendente',
+                            ])
+                            ->required(),
+                        Forms\Components\Textarea::make('description')
+                            ->label('Descrição')
+                            ->required()
+                            ->columnSpanFull(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Componente')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('code')
+                    ->label('Código Único')
+                    ->badge()
+                    ->color('info')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->label('Status')
+                    ->badge()
+                    ->color(function (string $state) : string {
+                        return match ($state) {
+                            'publicado' => 'success',
+                            'rascunho' => 'warning',
+                            'pendente' => 'danger',
+                        };
+                    }),
+                Tables\Columns\TextColumn::make('author.name')
+                    ->label('Autor')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubjects::route('/'),
+            'create' => Pages\CreateSubject::route('/create'),
+            'edit' => Pages\EditSubject::route('/{record}/edit'),
+        ];
+    }
+}
