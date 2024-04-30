a3218b8d23d9f1230c7b7d29b3053f0ddf0ad304
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-30 16:10:07 -0400

    Criação da migration geração da tabela Componentes Curriculares

---
+++ database/migrations/2024_04_30_141106_create_subjects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title');
+            $table->string('code')->unique();
+            $table->text('description');
+            $table->enum('status', ['publicado', 'rascunho', 'pendente'])->default('rascunho');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
