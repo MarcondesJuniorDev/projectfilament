13343e149c5ad347dab4869224f850de3a90e781
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-30 16:08:21 -0400

    Geração de Resources e suas paginas de CRUD para o model Categoria de Cursos

---
+++ app/Filament/Resources/CourseCategoryResource.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CourseCategoryResource\Pages;
+use App\Filament\Resources\CourseCategoryResource\RelationManagers;
+use App\Models\CourseCategory;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CourseCategoryResource extends Resource
+{
+    protected static ?string $model = CourseCategory::class;
+
+    protected static ?string $navigationGroup = "L M S";
+
+    protected static ?string $modelLabel = "Categoria de Curso";
+
+    protected static ?string $pluralModelLabel = "Categorias de Cursos";
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    protected static ?string $activeNavigationIcon = 'heroicon-s-tag';
+
+    protected static ?string $navigationParentItem = 'Cursos';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('Categoria')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('order')
+                    ->label('Ordem')
+                    ->required()
+                    ->mask('999')
+                    ->default(0),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('author.name')
+                    ->numeric()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('order')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCourseCategories::route('/'),
+            'create' => Pages\CreateCourseCategory::route('/create'),
+            'edit' => Pages\EditCourseCategory::route('/{record}/edit'),
+        ];
+    }
+}
