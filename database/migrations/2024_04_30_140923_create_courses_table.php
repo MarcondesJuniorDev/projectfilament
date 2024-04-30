3a3f1db27147e3403db974e47f58bec0a0d3221d
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-30 16:08:40 -0400

    Criação da migration geração da tabela Cursos

---
+++ database/migrations/2024_04_30_140923_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('course_categories')->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('summary');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->enum('status', ['publicado', 'rascunho', 'pendente'])->default('rascunho');
+            $table->boolean('featured')->default(false);
+            $table->boolean('featured_menu')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
