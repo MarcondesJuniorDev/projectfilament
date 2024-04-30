f3e441fd68f1bf68a35efc0612d2beb13f7c9091
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-30 16:07:03 -0400

    Criação da migration geração da tabela Categoria de Curso

---
+++ database/migrations/2024_04_30_140917_create_course_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->integer('order')->default(0)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_categories');
+    }
+};
