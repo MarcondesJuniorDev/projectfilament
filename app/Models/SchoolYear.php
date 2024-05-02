91a14c7ce332b5efc07c8036ab5610f33a62db6c
MarcondesJuniorDev <mncjdev@gmail.com>
2024-05-02 15:12:41 -0400

    Inclusão de relacionamento com Aulas e Inclusao de condição para deleção de dados

--- app/Models/SchoolYear.php
+++ app/Models/SchoolYear.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -22,12 +23,33 @@ class SchoolYear extends Model
         parent::boot();
 
         static::saving(function ($schoolYear) {
-            $schoolYear->author_id = auth()->id() ?? SchoolYear::factory()->create()->id;
+            $schoolYear->author_id = auth()->id() ?? User::factory()->create()->id;
+        });
+
+        static::deleting(function($schoolYear) {
+            if ($schoolYear->lesson()->count() > 0) {
+                Notification::make()
+                    ->title('Erro ao excluir Ano Letivo')
+                    ->body('Não é possível excluir um ano letivo que esteja vinculado a uma ou mais aulas.')
+                    ->color('danger')
+                    ->icon('heroicon-s-x-circle')
+                    ->iconColor('danger')
+                    ->status('error')
+                    ->duration(5000)
+                    ->inline()
+                    ->send();
+                return false;
+            }
         });
     }
 
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'author_id', 'id');
+    }
+
+    public function lesson(): HasOne
+    {
+        return $this->hasOne(Lesson::class);
     }
 }
