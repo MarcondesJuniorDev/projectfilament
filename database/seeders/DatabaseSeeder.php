67045c07113b5b23b9e5bff1052a3c9706ff2801
MarcondesJuniorDev <mncjdev@gmail.com>
2024-05-02 15:18:47 -0400

    Inclusao de factories e implementação de metodos para iniciar a geração de dados fake para testes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,12 @@ namespace Database\Seeders;
 use App\Models\Course;
 use App\Models\CourseCategory;
 use App\Models\Department;
+use App\Models\Lesson;
+use App\Models\LessonContent;
 use App\Models\Permission;
 use App\Models\Role;
 use App\Models\SchoolGrade;
+use App\Models\SchoolYear;
 use App\Models\Subject;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -24,14 +27,25 @@ class DatabaseSeeder extends Seeder
         $this->attachPermissionsToRoles();
         $this->attachRolesToUsers();
 
-        User::factory()->count(50)->create();
+        User::factory()->count(10)->create();
         Department::factory()->count(10)->create();
+
+        $this->attachUsersToDepartments();
+
         CourseCategory::factory()->count(10)->create();
         Course::factory()->count(10)->create();
         Subject::factory()->count(10)->create();
         SchoolGrade::factory()->count(10)->create();
+        $this->createSchoolYears();
+        $lessons = Lesson::factory()->count(10)->create();
 
-        $this->attachUsersToDepartments();
+        foreach ($lessons as $lesson) {
+            LessonContent::factory()->count(random_int(1, 5))->create([
+                'lesson_id' => $lesson->id,
+            ]);
+        }
+
+
     }
 
     private function createRoles(): void
@@ -110,5 +124,18 @@ class DatabaseSeeder extends Seeder
                 $users->random(rand(5, 10))->pluck('id')->toArray()
             );
         });
+    }
+
+    private function createSchoolYears(): void
+    {
+        $years = ['2020', '2021', '2022', '2023', '2024'];
+
+        foreach ($years as $year) {
+            SchoolYear::factory()->create([
+                'author_id' => User::query()->inRandomOrder()->first(),
+                'year' => $year,
+                'is_current' => $year >= 2023 ? 'yes' : 'no',
+            ]);
+        }
     }
 }
