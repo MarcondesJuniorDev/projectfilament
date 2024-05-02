3453725cbbad46cd43a5f62e61f051ad0928b61b
MarcondesJuniorDev <mncjdev@gmail.com>
2024-05-02 15:07:44 -0400

    Criação de migration para geração da tabela de conteudo de aulas

---
+++ database/migrations/2024_05_02_154758_create_lesson_contents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lesson_contents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->unsignedBigInteger('lesson_id');
+            $table->foreign('lesson_id')->references('id')->on('lessons')->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('is_main')->default(false);
+            $table->boolean('is_file')->default(false);
+            $table->string('content_type');
+            $table->string('content_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lesson_contents');
+    }
+};
