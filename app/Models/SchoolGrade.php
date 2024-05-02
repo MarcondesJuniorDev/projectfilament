1a31e3b3a38154dad3e282284235f9a4771598bd
MarcondesJuniorDev <mncjdev@gmail.com>
2024-05-02 08:55:56 -0400

    Criação e implementação da Model de Séries com seus relacionamentos

---
+++ app/Models/SchoolGrade.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SchoolGrade extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'author_id',
+        'course_id',
+        'subject_id',
+        'title',
+        'description',
+        'status',
+    ];
+
+    public static function boot(): void
+    {
+        parent::boot();
+
+        static::saving(function ($schoolGrade) {
+            $schoolGrade->author_id = auth()->id() ?? User::factory()->create()->id;
+        });
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class);
+    }
+}
